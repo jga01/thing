0546ace8a2c25649de1683abe9ecf18fce6c0a43
Jamestiago <tiagogoossenbra@gmail.com>
2023-08-08 00:07:04 -0300

    add collision between particles and add attractor mass to the center and add start of debug mode

--- src/main.c
+++ src/main.c
@@ -8,142 +8,186 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static sgp_vec2 points_buffer[4096];
-static const float PI = 3.14159265358979323846264338327f;
+typedef struct
+{
+    float x, y;
+    float vx, vy;
+    float radius;
+    sgp_color color;
+} Particle;
 
-static void draw_rects(void) {
+#define MAX_PARTICLES 100
+Particle particles[MAX_PARTICLES];
+
+int showVelocityVectors = false;
+
+// Define the gravitational constant
+#define GRAVITY_CONSTANT 0.10f
+#define FRICTION_CONSTANT 0.98f
+
+static void draw_vector(float x, float y, float dx, float dy, sgp_color color)
+{
+    sgp_push_transform();
+    sgp_set_color(color.r, color.g, color.b, color.a);
+    sgp_draw_line(x, y, x + dx, y + dy);
+    sgp_pop_transform();
+}
+
+static void draw_rects(void)
+{
     sgp_irect viewport = sgp_query_state()->viewport;
     int width = viewport.w, height = viewport.h;
-    int size = 64;
-    int hsize = size / 2;
     float time = sapp_frame_count() / 60.0f;
-    float t = (1.0f+sinf(time))/2.0f;
+    float t = (1.0f + sinf(time)) / 2.0f;
 
-    // left
-    sgp_push_transform();
-    sgp_translate(width*0.25f - hsize, height*0.5f - hsize);
-    sgp_translate(0.0f, 2*size*t - size);
-    sgp_set_color(t, 0.3f, 1.0f-t, 1.0f);
-    sgp_draw_filled_rect(0, 0, size, size);
-    sgp_pop_transform();
+    for (int i = 0; i < MAX_PARTICLES; i++)
+    {
+        sgp_push_transform();
+        // printf("[%d] %f %f %f %f\n", i, particles[i].color.r, particles[i].color.g, particles[i].color.b, particles[i].color.a);
+        sgp_set_color(particles[i].color.r / 255.0f, particles[i].color.g / 255.0f, particles[i].color.b / 255.0f, 1.0f);
+        sgp_draw_filled_rect(particles[i].x - particles[i].radius, particles[i].y - particles[i].radius,
+                             particles[i].radius * 2.0f, particles[i].radius * 2.0f);
+        sgp_pop_transform();
 
-    // middle
-    sgp_push_transform();
-    sgp_translate(width*0.5f - hsize, height*0.5f - hsize);
-    sgp_rotate_at(time, hsize, hsize);
-    sgp_set_color(t, 1.0f - t, 0.3f, 1.0f);
-    sgp_draw_filled_rect(0, 0, size, size);
-    sgp_pop_transform();
-
-    // right
-    sgp_push_transform();
-    sgp_translate(width*0.75f - hsize, height*0.5f - hsize);
-    sgp_scale_at(t + 0.25f, t + 0.5f, hsize, hsize);
-    sgp_set_color(0.3f, t, 1.0f - t, 1.0f);
-    sgp_draw_filled_rect(0, 0, size, size);
-    sgp_pop_transform();
-}
-
-static void draw_points(void) {
-    sgp_set_color(1.0f, 1.0f, 1.0f, 1.0f);
-    sgp_irect viewport = sgp_query_state()->viewport;
-    int width = viewport.w, height = viewport.h;
-    unsigned int count = 0;
-    for(int y=64;y<height-64 && count < 4096;y+=8) {
-        for(int x=64;x<width-64 && count < 4096;x+=8) {
-            sgp_vec2 v = {(float)x,(float)y};
-            points_buffer[count++] = v;
+        if (showVelocityVectors)
+        {
+            // Visualize the velocity, not the attraction force
+            draw_vector(particles[i].x, particles[i].y, particles[i].vx * 10, particles[i].vy * 10,
+                        (sgp_color){0.0f, 0.0f, 1.0f, 1.0f}); // Blue vector for velocity
         }
     }
-    sgp_draw_points(points_buffer, count);
 }
 
-static void draw_lines(void) {
-    sgp_set_color(1.0f, 1.0f, 1.0f, 1.0f);
-    unsigned int count = 0;
-    sgp_irect viewport = sgp_query_state()->viewport;
-    sgp_vec2 c = {viewport.w / 2.0f, viewport.h / 2.0f};
-    points_buffer[count++] = c;
-    for(float theta = 0.0f; theta <= PI*8.0f; theta+=PI/16.0f) {
-        float r = 10.0f*theta;
-        sgp_vec2 v = {c.x + r*cosf(theta), c.y + r*sinf(theta)};
-        points_buffer[count++] = v;
-    }
-    sgp_draw_lines_strip(points_buffer, count);
-}
-
-static void draw_triangles(void) {
-    sgp_irect viewport = sgp_query_state()->viewport;
-    int width = viewport.w, height = viewport.h;
-    float hw = width * 0.5f;
-    float hh = height * 0.5f;
-    float w = height*0.2f;
-    float ax = hw - w, ay = hh + w;
-    float bx = hw,     by = hh - w;
-    float cx = hw + w, cy = hh + w;
-    sgp_set_color(1.0f, 0.0f, 1.0f, 1.0f);
-    sgp_push_transform();
-    sgp_translate(-w*1.5f, 0.0f);
-    sgp_draw_filled_triangle(ax, ay, bx, by, cx, cy);
-    sgp_translate(w*3.0f, 0.0f);
-    unsigned int count = 0;
-    float step = (2.0f*PI)/6.0f;
-    for(float theta = 0.0f; theta <= 2.0f*PI + step*0.5f; theta+=step) {
-        sgp_vec2 v = {hw + w*cosf(theta), hh - w*sinf(theta)};
-        points_buffer[count++] = v;
-        if(count % 3 == 1) {
-            sgp_vec2 u = {hw, hh};
-            points_buffer[count++] = u;
-        }
-    }
-    sgp_set_color(0.0f, 1.0f, 1.0f, 1.0f);
-    sgp_draw_filled_triangles_strip(points_buffer, count);
-    sgp_pop_transform();
-}
-
-static void frame(void) {
+static void frame(void)
+{
     // begin draw commands queue
     int width = sapp_width(), height = sapp_height();
     sgp_begin(width, height);
 
-    int hw = width / 2;
-    int hh = height / 2;
+    for (int i = 0; i < MAX_PARTICLES; i++)
+    {
+        particles[i].x += particles[i].vx;
+        particles[i].y += particles[i].vy;
+
+        // Apply basic collision with screen boundaries
+        if (particles[i].x < 0 || particles[i].x > width)
+        {
+            particles[i].vx *= -1;
+        }
+        if (particles[i].y < 0 || particles[i].y > height)
+        {
+            particles[i].vy *= -1;
+        }
+
+        particles[i].vx *= FRICTION_CONSTANT;
+        particles[i].vy *= FRICTION_CONSTANT;
+
+        for (int j = i + 1; j < MAX_PARTICLES; j++)
+        {
+            float dx = particles[j].x - particles[i].x;
+            float dy = particles[j].y - particles[i].y;
+            float distanceSquared = dx * dx + dy * dy;
+
+            if (distanceSquared < (particles[i].radius + particles[j].radius) * (particles[i].radius + particles[j].radius))
+            {
+                // Collisions detected, adjust velocities
+                float overlap = particles[i].radius + particles[j].radius - sqrt(distanceSquared);
+                float angle = atan2(dy, dx);
+
+                particles[i].vx -= overlap * cos(angle);
+                particles[i].vy -= overlap * sin(angle);
+                particles[j].vx += overlap * cos(angle);
+                particles[j].vy += overlap * sin(angle);
+            }
+        }
+
+        // Position and mass of the fixed point of attraction
+        float attractorX = sapp_width() / 2.0f;
+        float attractorY = sapp_height() / 2.0f;
+        float attractorMass = 100.0f;
+        float attractorRadius = 100.0f;
+
+        // Flag to control whether gravitational attraction is enabled or not
+        bool gravitationalAttractionEnabled = true;
+
+        if (gravitationalAttractionEnabled)
+        {
+            for (int j = 0; j < MAX_PARTICLES; j++)
+            {
+                if (j == i)
+                    continue; // Skip calculations for the same particle
+
+                float dx = attractorX - particles[i].x;
+                float dy = attractorY - particles[i].y;
+                float distanceSquared = dx * dx + dy * dy;
+                float minDistance = particles[i].radius + attractorRadius;
+
+                if (distanceSquared < minDistance * minDistance)
+                {
+                    // Particle is within the combined radius, adjust position
+                    float distance = sqrt(distanceSquared);
+                    float penetration = minDistance - distance;
+                    float angle = atan2(dy, dx);
+
+                    // Move the particle outside the combined radius
+                    particles[i].x -= penetration * cos(angle);
+                    particles[i].y -= penetration * sin(angle);
+
+                    // Apply attraction force
+                    float normalizedDistance = 1.0f - (distance / attractorRadius);
+                    float easedForce = GRAVITY_CONSTANT * attractorMass * normalizedDistance * normalizedDistance;
+                    particles[i].vx += easedForce * cos(angle);
+                    particles[i].vy += easedForce * sin(angle);
+                }
+                else
+                {
+                    // Particle is outside attractor range, apply regular attraction
+                    float force = (GRAVITY_CONSTANT * attractorMass) / distanceSquared;
+                    float angle = atan2(dy, dx);
+
+                    particles[i].vx += force * cos(angle);
+                    particles[i].vy += force * sin(angle);
+                }
+            }
+        }
+    }
 
     // draw background
     sgp_set_color(0.05f, 0.05f, 0.05f, 1.0f);
     sgp_clear();
     sgp_reset_color();
 
-    // top left
-    sgp_viewport(0, 0, hw, hh);
+    sgp_viewport(0, 0, width, height);
     sgp_set_color(0.1f, 0.1f, 0.1f, 1.0f);
     sgp_clear();
     sgp_reset_color();
     sgp_push_transform();
-    sgp_translate(0.0f, -hh / 4.0f);
     draw_rects();
     sgp_pop_transform();
+
     sgp_push_transform();
-    sgp_translate(0.0f, hh / 4.0f);
-    sgp_scissor(0, 0, hw, 3.0f*hh / 4.0f);
-    draw_rects();
-    sgp_reset_scissor();
-    sgp_pop_transform();
+    sgp_set_color(1.0f, 0.0f, 0.0f, 1.0f); // Red color for the circle
+    float attractorRadius = 100.0f;
+    int numSides = 32;
+    float angleIncrement = (2.0f * M_PI) / numSides;
+    float attractorX = sapp_width() / 2.0f;
+    float attractorY = sapp_height() / 2.0f;
 
-    // top right
-    sgp_viewport(hw, 0, hw, hh);
-    draw_triangles();
+    for (int i = 0; i < numSides; i++)
+    {
+        float angle = angleIncrement * i;
+        float nextAngle = angle + angleIncrement;
 
-    // bottom left
-    sgp_viewport(0, hh, hw, hh);
-    draw_points();
+        float x0 = attractorX + attractorRadius * cosf(angle);
+        float y0 = attractorY + attractorRadius * sinf(angle);
+        float x1 = attractorX + attractorRadius * cosf(nextAngle);
+        float y1 = attractorY + attractorRadius * sinf(nextAngle);
 
-    // bottom right
-    sgp_viewport(hw, hh, hw, hh);
-    sgp_set_color(0.1f, 0.1f, 0.1f, 1.0f);
-    sgp_clear();
+        // Draw the triangle
+        sgp_draw_filled_triangle(attractorX, attractorY, x0, y0, x1, y1);
+    }
     sgp_reset_color();
-    draw_lines();
+    sgp_pop_transform();
 
     // dispatch draw commands
     sg_pass_action pass_action = {0};
@@ -154,11 +198,13 @@ static void frame(void) {
     sg_commit();
 }
 
-static void init(void) {
+static void init(void)
+{
     // initialize Sokol GFX
     sg_desc sgdesc = {.context = sapp_sgcontext()};
     sg_setup(&sgdesc);
-    if(!sg_isvalid()) {
+    if (!sg_isvalid())
+    {
         fprintf(stderr, "Failed to create Sokol GFX context!\n");
         exit(-1);
     }
@@ -166,25 +212,54 @@ static void init(void) {
     // initialize Sokol GP
     sgp_desc sgpdesc = {0};
     sgp_setup(&sgpdesc);
-    if(!sgp_is_valid()) {
+    if (!sgp_is_valid())
+    {
         fprintf(stderr, "Failed to create Sokol GP context: %s\n", sgp_get_error_message(sgp_get_last_error()));
         exit(-1);
     }
+
+    for (int i = 0; i < MAX_PARTICLES; i++)
+    {
+        particles[i].x = rand() % sapp_width();
+        particles[i].y = rand() % sapp_height();
+        particles[i].vx = ((float)rand() / RAND_MAX - 0.5f) * 2.0f;
+        particles[i].vy = ((float)rand() / RAND_MAX - 0.5f) * 2.0f;
+        particles[i].radius = 5.0f;
+        particles[i].color = (sgp_color){rand() % 256, rand() % 256, rand() % 256, 1.0};
+    }
 }
 
-static void cleanup(void) {
+static void cleanup(void)
+{
     sgp_shutdown();
     sg_shutdown();
 }
 
-sapp_desc sokol_main(int argc, char* argv[]) {
+static void event(const sapp_event *event)
+{
+    if (event->type == SAPP_EVENTTYPE_KEY_DOWN)
+    {
+        switch (event->key_code)
+        {
+        case SAPP_KEYCODE_S:
+            showVelocityVectors = !showVelocityVectors;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+sapp_desc sokol_main(int argc, char *argv[])
+{
     (void)argc;
     (void)argv;
     return (sapp_desc){
         .init_cb = init,
         .frame_cb = frame,
         .cleanup_cb = cleanup,
-        .window_title = "Primitives (Sokol GP)",
+        .event_cb = event,
+        .window_title = "Collision Detection",
         .sample_count = 4,
     };
 }
